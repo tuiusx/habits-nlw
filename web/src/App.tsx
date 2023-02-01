317431786779ac4c0dd209c0ade83342de9a740a
tuiusx <tuiusx@icloud.com>
2023-01-31 22:42:43 -0300

    header

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,25 @@
 import './styles/global.css';
+import { Plus } from "phosphor-react"
 
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import logoImage from "./assets/logo.svg"
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {
   return (
-    <h1>Hello Word</h1>
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+        <div className='w-full max-w-3xl mx-auto flex items-center justify-between'>
+          <img src={logoImage} alt="Habits" />
+          
+          <button 
+            type='button'
+            className='border border-violet-500 font-semibold rounded-lg px-6 py-4 flex items-center gap-3 hover:border-violet-300'
+          >
+            <Plus size={20} className="text-violet-500"/>
+            Novo Hábito
+          </button>
+
+        </div>
+      </div>
+    </div>
   )
 }
-
-export default App
